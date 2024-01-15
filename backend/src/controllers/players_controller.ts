475144d1f113cda5f2200bf541d878adb3bfbc97
Filipe Damaceno <filipedamaceno.dev@gmail.com>
2024-01-15 18:45:58 -0300

    crud de player-update

--- backend/src/controllers/players_controller.ts
+++ backend/src/controllers/players_controller.ts
@@ -8,24 +8,31 @@ import nba from "nba";
 
 export class PlayerController {
 	static async createPlayer(req: Request, res: Response) {
+		
 		const { name, number, height, weight, team, position, country } = req.body;
 		const playerRepository = AppDataSource.getRepository(Player);
-		const player = new Player();
-		player.name = name;
-		player.number = number;
-		player.height = height;
-		player.weight = weight;
-		player.team = team;
-		player.position = position;
-		player.country = country;
-
-		await playerRepository.save(player);
-		res.status(201).json({
-			status: "success",
-			data: player,
+		const existingPlayer = await playerRepository.findOne({
+			where: {name: name}
 		});
-	}
 
+		if(!existingPlayer) {
+			const player = new Player();
+			player.name = name;
+			player.number = number;
+			player.height = height;
+			player.weight = weight;
+			player.team = team;
+			player.position = position;
+			player.country = country;
+			await playerRepository.save(player);
+			res.status(201).json({
+				status: "success",
+				data: player,
+			});
+		} else {
+			res.status(404).json({ message: "Jogador já está cadastrado" });
+		}
+	}
 	
 	static async getPlayersById(req: Request, res: Response) {
 		const playerID = req.params.id;
@@ -47,6 +54,31 @@ export class PlayerController {
 			return res.status(500).json({ message: "Erro interno do servidor" });
 		}
 	}
+
+	static async updatePlayer(req: Request, res: Response) {
+		const playerID = req.params.id;
+		const { name, number, height, weight, team, position, country } = req.body;
+		const playerRepository = AppDataSource.getRepository(Player);
+		
+		const player = await playerRepository.findOne({
+			where: {
+				id: playerID,
+			},
+		});
+		if (player) {
+			player.name = name;
+			player.number = number;
+			player.height = height;
+			player.weight = weight;
+			player.team = team;
+			player.position = position;
+			player.country = country;
+			await playerRepository.save(player);
+			res.status(200).json({ message: "Jogador encontrado e atualizado com sucesso" });
+		} else {
+			res.status(404).json({ message: "Jogador não encontrado" });
+		}
+	}
 	
 	static async deletePlayer(req: Request, res: Response) {
 		const playerID = req.params.id;
@@ -59,20 +91,20 @@ export class PlayerController {
 		});
 		if (player) {
 			await playerRepository.remove(player);
-			res.status(200).json({ message: "Jogador encontrado, removendo..." });
-			return res.status(200).json({ message: "Jogador removido com sucesso" });
+			res.status(200).json({ message: "Jogador encontrado e removido com sucesso" });
+			
 		} else {
 			res.status(404).json({ message: "Jogador não encontrado" });
 		}
 	}
 	
-	// static async getPlayers(req: Request, res: Response) {
-	// 	const teamID = 16;
-	// 	const team = await sdv.nba.getTeamPlayers(teamID);
+	static async getPlayers(req: Request, res: Response) {
+		const teamID = req.params.id;
+		const team = await nba.stats.playerInfo({ TeamID: teamID });
 		
-	// 	res.status(200).json({
-	// 		status: "success",
-	// 		data: team,
-	// 	});
-	// }
+		res.status(200).json({
+			status: "success",
+			data: team,
+		});
+	}
 }
--- backend/src/routes/player.ts
+++ backend/src/routes/player.ts
@@ -2,17 +2,14 @@ import * as express from "express";
 import { PlayerController } from "../controllers/players_controller";
 const Router = express.Router();
 
+Router.post("/players", PlayerController.createPlayer);
+
 Router.get("/players", PlayerController.getPlayers);
 
 Router.get("/players/:id", PlayerController.getPlayersById);
 
-Router.post(
-  "/players", PlayerController.createPlayer);
+Router.put("/players/:id", PlayerController.updatePlayer);
 
-// Router.put(
-//    "/players/:id", PlayerController.updatePlayer);
-
- Router.delete(
-   "/players/:id", PlayerController.deletePlayer);
+ Router.delete("/players/:id", PlayerController.deletePlayer);
 
 export { Router as playerRouter };
